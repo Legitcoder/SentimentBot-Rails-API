d0c991a0fd4893f44e1e60d83815d0448fefb93f
Moin Uddin <U_Moin75@yahoo.com>
2019-01-12 19:01:21 -0500

    AddPhoneToUsers attribute migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_12_042801) do
+ActiveRecord::Schema.define(version: 2019_01_13_000043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2019_01_12_042801) do
     t.bigint "team_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone"
     t.index ["team_id"], name: "index_users_on_team_id"
   end
 
